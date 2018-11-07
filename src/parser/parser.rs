ab6bdfe260534e3c1dd9e983c19c3e50e99f83c5
Derek Foundoulis <foundoulis@mymail.mines.edu>
2018-11-07 14:29:21 -0700

    Regex strings now work.

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,7 +1,7 @@
 
 extern crate regex;
 
-use regex::RegexSet;
+use self::regex::RegexSet;
 
 pub enum ControlToken {
     LParen,
@@ -11,18 +11,19 @@ pub enum ControlToken {
 
 fn lex(code: String) -> Vec<ControlToken> {
     let reglex = RegexSet::new(&[
-        r"\(", 
-        r"\)",
-        r"(?:-?[0-9]+[\.][0-9]*)|(?:-?[0-9]*[\.][0-9]+)",
-        r"-?[0-9]+",
-        r"\s",
-        r""(?:[^"\\]|\\.)*"",
-        r"^[#!].*",
-        r";.*",
-        r"[^.\s\'\"\(\);][^\s\'\"\(\);]*",
-        r".*"
-    ]);
-    reglex.matches(code).into_iter().collect()
+        r###"\("###,  // 1
+        r###"\)"###,  // 2
+        r###"\'"###,  // 3 Control sequence ()'
+        r###"(?:-?[0-9]+[\.][0-9]*)|(?:-?[0-9]*[\.][0-9]+)"###,
+        r###"-?[0-9]+"###,  // 5 Integer
+        r###"\s"###, // 6 Whitespace
+        r###""(?:[^"\\]|\\.)*""###,  // 7 String
+        r###"^[#!].*"###,  // 8 comments
+        r###";.*"###,  // 9 other comments
+        r###"[^.\s\'\"\(\);][^\s\'\"\(\);]*"###,  // 10 symbols
+        r###".*"### // 11 Syntax error for anything else.
+    ]).unwrap();
+    reglex.matches(&code).into_iter().collect()
 }
 
 fn parse(tokens: Vec<ControlToken>) -> () {
